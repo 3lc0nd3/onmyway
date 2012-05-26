3066c70dcb5a87b3ac63f55c4ee0e85a35d8593e
Edward Ramirez Avella <elramireza@gmail.com>
2012-05-26 09:23:05 -0500

    call

--- src/main/java/co/com/elramireza/calls/fb/Facebook.java
+++ src/main/java/co/com/elramireza/calls/fb/Facebook.java
@@ -1,7 +1,5 @@
 package co.com.elramireza.calls.fb;
 
-import com.visural.common.StringUtil;
-
 /**
  * Created by Edward L. Ramirez A.
  * cel 300 554 3367
@@ -12,14 +10,15 @@ import com.visural.common.StringUtil;
  */
 public class Facebook {
     // get these from your FB Dev App
-    private static final String api_key = "MYAPIKEY";
-    private static final String secret = "MYSECRETKEY";
-    private static final String client_id = "MYCLIENTID";
+    private static final String api_key = "test";
+    private static final String secret = "f2d4b8b3dd97e69950174b757b0cba4d";
+    private static final String client_id = "251218434898090";
 
     // set this to your servlet URL for the authentication servlet/filter
-    private static final String redirect_uri = "http://www.onmydoorstep.com.au/fbauth";
+    private static final String redirect_uri = "http://p4s.co/onmyway/callbak.jsp";
     /// set this to the list of extended permissions you want
-    private static final String[] perms = new String[] {"publish_stream", "email"};
+//    private static final String[] perms = new String[] {"publish_stream", "email"};
+    private static final String scope = "email,offline_access,user_about_me,user_birthday,read_friendlists";
 
     public static String getAPIKey() {
         return api_key;
@@ -32,7 +31,7 @@ public class Facebook {
     public static String getLoginRedirectURL() {
         return "https://graph.facebook.com/oauth/authorize?client_id=" +
                 client_id + "&display=page&redirect_uri=" +
-                redirect_uri+"&scope="+ StringUtil.delimitObjectsToString(",", perms);
+                redirect_uri+"&scope="+ scope;
     }
 
     public static String getAuthURL(String authCode) {
@@ -40,5 +39,18 @@ public class Facebook {
                 client_id+"&redirect_uri=" +
                 redirect_uri+"&client_secret="+secret+"&code="+authCode;
     }
+
+    public static String getScope(){
+        return scope;
+    }
+
+    public static String getClientId(){
+        return client_id;
+    }
+
+    public static String getRedirectUri(){
+        return redirect_uri;
+    }
+
 
 }
--- src/main/webapp/signin.jsp
+++ src/main/webapp/signin.jsp
@@ -1,18 +1,11 @@
-
+<%@ page import="co.com.elramireza.calls.fb.Facebook" %>
 <%
-
-    
-    String SCOPE = "email,offline_access,user_about_me,user_birthday,read_friendlists";
-    String REDIRECT_URI = "http://p4s.co/onmyway/callbak.jsp";
-    String CLIENT_ID = "251218434898090";
-    String APP_SECRET = "f2d4b8b3dd97e69950174b757b0cba4d";
     String DIALOG_OAUTH = "https://www.facebook.com/dialog/oauth";
     String ACCESS_TOKEN = "https://graph.facebook.com/oauth/access_token";
 
-
-    String s = DIALOG_OAUTH + "?client_id=" + CLIENT_ID +
-            "&redirect_uri=" + REDIRECT_URI +
-            "&scope=" + SCOPE;
+    String s = DIALOG_OAUTH + "?client_id=" + Facebook.getClientId() +
+            "&redirect_uri=" + Facebook.getRedirectUri() +
+            "&scope=" + Facebook.getScope();
     System.out.println("s = " + s);
     response.sendRedirect(s);
 
