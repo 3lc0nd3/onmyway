ce02bff3de278864819fd52ee4c891a2419e0871
Edward Ramirez Avella <elramireza@gmail.com>
2012-05-26 12:45:32 -0500

    call

---
+++ src/main/java/co/com/elramireza/calls/controller/signin.java
@@ -0,0 +1,19 @@
+package co.com.elramireza.calls.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by Edward L. Ramirez A.
+ * cel 300 554 3367
+ * email elramireza@gmail.com
+ * User: usuariox
+ * Date: May 26, 2012
+ * Time: 12:35:24 PM
+ */
+
+@Controller
+public class signin {
+//    @Autowired
+//    private IFacade facade;
+}
