6fedf23515f8f6fcdbfa35934e1fccc58d7420d5
Edward <elramireza@gmail.com>
2012-06-13 09:08:52 -0500

    call

--- src/main/java/co/com/elramireza/calls/fb/Facebook.java
+++ src/main/java/co/com/elramireza/calls/fb/Facebook.java
@@ -59,14 +59,14 @@ public class Facebook {
     }
 
     public static String readURL(URL url) throws IOException {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            InputStream is = url.openStream();
-            int r;
-            while ((r = is.read()) != -1) {
-                baos.write(r);
-            }
-            return new String(baos.toByteArray());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        InputStream is = url.openStream();
+        int r;
+        while ((r = is.read()) != -1) {
+            baos.write(r);
         }
+        return new String(baos.toByteArray());
+    }
 
 
 }
